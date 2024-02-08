4fce7574251be66a8f9f075b60c5cd4e830b138d
Arvik1982 <phone.kulikovarseniy@gmail.com>
2024-02-08 16:24:57 +0300

    080224

---
+++ src/components/Authorization/Register.jsx
@@ -0,0 +1,87 @@
+
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import styles from'./register.module.css'
+import logoModal from '../../img/logo_modal.png'
+import { getTokens, registration } from '../../api'
+import getInputValue from '../../functions/getInputValue'
+import { setError } from '../../store/reducers/sliceError'
+import createUserUid from '../../functions/createUid'
+import { setUserData} from '../../store/reducers/sliceReg'
+import InputMail from './inputMail'
+import InputPass from './inputPass'
+
+export default function Register(){
+
+  const userMail = useSelector(state=>state.authRedux.userMail)
+  const userPassword = useSelector(state=>state.authRedux.password)
+  const userPassword2 = useSelector(state=>state.authRedux.password2)
+  const userName = useSelector(state=>state.authRedux.userName)
+  const userSurName = useSelector(state=>state.authRedux.userSurName)
+  const userCity= useSelector(state=>state.authRedux.userCity)
+  const error= useSelector(state=>state.errorRedux.error)
+  const userId=createUserUid()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+return(
+
+<form className={styles.modal__form_login} id="formLogUp" action="#">
+<div className={styles.modal__logo}>
+    <img src={logoModal} alt="logo"/>
+</div>
+<InputMail/>
+<InputPass/>
+<input onChange={(event)=>{ getInputValue(event, dispatch, 'password2')}}
+ className={`${styles.modal__input} ${styles.password_double}`} 
+ type="password" 
+ name="password2" 
+ id="passwordSecond" 
+ placeholder="Повторите пароль"/>
+
+<input onChange={(event)=>{ getInputValue(event, dispatch, 'userName')}}
+ className={`${styles.modal__input} ${styles.first_name}`} 
+ type="text" 
+ name="first-name" 
+ id="first-name" 
+ placeholder="Имя (необязательно)"/>
+
+<input onChange={(event)=>{ getInputValue(event, dispatch, 'userSurName')}}
+ className={`${styles.modal__input} ${styles.first_last}`} 
+ type="text" name="first-last" 
+ id="first-last" 
+ placeholder="Фамилия (необязательно)"/>
+
+
+<input onChange={(event)=>{ getInputValue(event, dispatch, 'userCity')}}
+className={`${styles.modal__input} ${styles.city}`} 
+type="text" 
+name="city" 
+id="city" 
+placeholder="Город (необязательно)"/>
+{error&&<div style={{color:'red', position:'absolute', bottom:'17%', marginBottom:'8px'}}>{error}</div>}
+<button 
+onClick={
+    ()=>{userPassword!==userPassword2?dispatch(setError('Пароли не совпадают')):
+    registration(
+    userMail,
+    userPassword,     
+    userName,
+    userSurName,
+    userCity,
+    userId
+    )
+    .then((data)=>{dispatch(setUserData(data))}).then(()=>{
+        navigate('/profile')
+    }).then(getTokens(userMail,userPassword))
+    .catch((newError)=> dispatch(setError(newError.message)))
+
+}
+} 
+    type='button' 
+    className={styles.modal__btn_signup_ent} 
+    id="SignUpEnter">
+    <a href="#">Зарегистрироваться</a> </button>
+</form>
+
+)}
